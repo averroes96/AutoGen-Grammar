1356cba4a9047ae9e2261c986cbb69867636d1fe
Ada Meceffeuk <46904388+averroes96@users.noreply.github.com>
2021-02-28 00:27:08 +0100

    Refresh

--- main.py
+++ main.py
@@ -4,17 +4,6 @@ from nltk import FreqDist,defaultdict
 import os,re,time
 from operator import itemgetter
 
-def to_POSTagged_corpus(corpus): # Function takes as a parameter an organized corpus and turn it into a tagged one string
-
-    data_file = ""
-
-    for sent in corpus:
-        for token in sent:
-            data_file += f"{token[0]}/{token[1]} "
-        data_file += "\n"
-
-    return data_file
-
 def only_tags(sent) -> str: # Function to get only the tags from a tagged sentence
     tokens = sent.split()
     temp = ""
@@ -77,26 +66,15 @@ def substitute(sents, rule):
 
     rule_text = rule[1][0] + " " + rule[1][1]
     rule_text_regex = rule_text + r"\s"
-    #rule_text_regex = check(rule_text_regex)
-    #print(rule_text)
-    #print(rule_text)
+
     for i in range(0, len(sents)-1):
-        #print(sent)
         if rule_text in sents[i]:
-            #print(rule[0])
-            #sent.replace(rule_text, rule[0])
+
             if "$" or "*" in rule_text:
-                #print("Somethin in the way")
-                #print(rule[0])
-                #print(type(sents[i]))
-                #x = str(sents[i])
                 print(rule_text)
                 sents[i] = sents[i].replace(rule_text, rule[0])
             else:
-                #print(rule[0])
-                #print(sents[i])
                 sents[i] = re.sub(rule_text_regex, rule[0] + " ", sents[i])
-            #print(sent)
 
     return sents
 
@@ -119,6 +97,8 @@ def check_terminals(current, all):
 
     return False
 
+# Main Algorithm
+
 def run(path):
 
     all_tags = get_corpus_tags(path) # get all the tags of the original corpus
@@ -137,26 +117,12 @@ def run(path):
         current_tags = get_current_tags(current_sents)
         if not check_terminals(current_tags, all_tags):
             has_terminals = False
-        
-        #if "$" in rule[1][1]:
-        #    for sent in current_sents:
-        #        print(sent)
-        #   has_terminals = False
 
         print(f"{rule[0]} => {rule[1]}")
         cpt+=1
         print("=" * 96)
 
     return rules
-
-#sentence = "	The/at Fulton/np-tl County/nn-tl Grand/jj-tl Jury/nn-tl said/vbd Friday/nr an/at investigation/nn of/in Atlanta's/np$ recent/jj primary/nn election/nn produced/vbd ``/`` no/at evidence/nn ''/'' that/cs any/dti irregularities/nns took/vbd place/nn ./."
-
- # Calculate the frequency distribution of each ngram
-
-#sorted_tag_counts = sorted(fd.items(), key=itemgetter(1), reverse=True)
-#print([[(token,freq) for (token, freq) in sorted_tag_counts[:20]]])
-
-#print(tags)
 
 extracted_rules = run("brown\\ca01")
 
