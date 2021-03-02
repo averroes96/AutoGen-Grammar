5bb9ec94c14885099cdf7df048d8bd6a474c370b
Ada Meceffeuk <46904388+averroes96@users.noreply.github.com>
2021-03-03 00:11:26 +0100

    Also Rule Extraction

--- app.py
+++ app.py
@@ -1,5 +1,99 @@
+from nltk import ngrams
+from nltk.corpus import brown
+from nltk import FreqDist,defaultdict
+import os,re,time
+from operator import itemgetter
+
+def only_tags(sent) -> str: # Function to get only the tags from a tagged sentence
+    tokens = sent.split()
+    temp = ""
+    for token in tokens:
+        temp += token.split("/")[1] + " "
+
+    return temp
+
+def get_corpus_tags(path)->str:
+
+    sentences = open(path)
+    tags = ""
+
+    for sentence in sentences:
+        sentence = only_tags(sentence)
+        tags += sentence + " "
+
+    fd = FreqDist(tags.split())
+
+    return list(fd)
+
+# Function to extract the all possible ngrams from a list of sents
+#if thers 2 similar sentences it will not extract the nçgrams for the 2nd centence 
+def extract_ngrams(sents)->list:
+
+    mygrams = []
+    for sentence in sents:
+        for i in range(2, len(sentence) - 1):
+            mygrams += ngrams(sentence.split(), i)
+            
+            # I want to go to school
+            # I want 
+
+    return mygrams
+
+# get all the sentences of a file | path:str = path of the file
+##################################################################################################
+def get_sents(path)->str:
+
+    file = open(path).read().strip().split("\n")
+    sents = []
+
+    for line in file:
+        if line != "":############# changes here ###########
+            line = only_tags(line)
+            sents.append(line)
+
+    return sents
+
+# Substitution, replace in a list of sentences the tag by the rule name (needs revision)
+def substitute(sents, rule):
+
+    rule_text = rule[1][0] + " " + rule[1][1]
+    rule_text_regex = rule_text + r"\s"
+
+    for i in range(0, len(sents)-1):
+        if rule_text in sents[i]:
+
+            if "$" or "*" in rule_text:
+                print(rule_text)
+                sents[i] = sents[i].replace(rule_text, rule[0])
+            else:
+                sents[i] = re.sub(rule_text_regex, rule[0] + " ", sents[i])
+
+    return sents
+
+# get the current tags
+#its the same as only tags but with list of sentences instead of path 
+def get_current_tags(sents):
+
+    tags = ""
+    for sent in sents:
+        sent = sent.split()
+        for tag in sent:
+            tags += tag + " "
+
+    return list(FreqDist(tags.split()))
+
+#check if the tag belongs to the corpus tags 
+def check_terminals(current, all):
+
+    for tag1 in current:
+        for tag2 in all:
+            if tag1 == tag2:
+                return True
+
+    return False
+
 rule_base = []
-#replace the gram by her rule name
+#replace the gram by its rule name
 def rplc(sents,gram,rule_name):
     #the sentences are already only tags
     r="" #the string of the grame to replace
@@ -9,8 +103,8 @@ def rplc(sents,gram,rule_name):
     snt=[]
     for s in sents:
         stri=s.replace(r,rule_name+" ")
-        if(len(stri.split())>1): snt.append(stri)
-        else : rule_base.append(stri)    
+        if(len(stri.split()) > 1): snt.append(stri)
+        else : rule_base.append(stri.strip())    
     return snt
 
 #extraction de regles
@@ -19,36 +113,29 @@ def mm (path):
     current_sents = get_sents(path)
     cpt=0
     while (len(current_sents)!=0):
-        grams=extract_ngrams(current_sents)#get the n_grams
+        grams = extract_ngrams(current_sents)#get the n_grams
         fd = FreqDist(grams)
         r = fd.max()#r has the most frequente gram
-        rules["NT"+str(cpt)]=r
-        current_sents=rplc(current_sents,r,"NT"+str(cpt))#replace the gram by her rule name
-        cpt+=1
+        rules["NT" + str(cpt)] = r
+        current_sents = rplc(current_sents,r,"NT"+str(cpt))#replace the gram by her rule name
+        cpt += 1
     return rules
-rules =mm("brown/ca01")
 
-(r not in racine or r==rule)): #and (r not in rule_base or r==rule)):
-                    b=True
-                    #replace r with its tags
-                    rl = rl+st(rules[r])
-                else : rl = rl + r + " "
-        rule_tag[rule]=rl
-    if(b):return rec_tag(rule_tag,rules,racine)
-    return rule_tag
-tag_rules=rec_tag(dict_rule,rules,racine)
-    
-def test_gram (sentence,grammer):
-    b=False
-    for r in grammer:
-        if(r.strip()==sentence.strip()):return True
-        if(grammer[r].strip() in sentence.strip()):
-            b=True
-            sentence = sentence.replace(grammer[r].strip(),r)
-    if(b==True):return test_gram (sentence,grammer)
-    return False
+def rreplace(s, old, new, occurrence):
+    li = s.rsplit(old, occurrence)
+    return new.join(li)
 
-s ="in ap nns , at nn vbd cs : ( cd ) "
-print(test_gram(s,tag_rules))
+rules = mm("brown/ca01")
+#at this point ull have a dict named rules that containes the noun of the rule or sub_rule with its tag
+# and a list names rule_base that contains the diff racine de grammer
 
-    
+print(rule_base)
+grammar_file = open("grammar.cfg", "w")
+for rule in rule_base:
+    #print(rule)
+    if rule.startswith("NT"):
+        grammar_file.write(f"{rule} -> ")
+        terminals = ""
+        for terminal in rules[rule]:
+            terminals = f"{terminals} {terminal}"
+        grammar_file.write(f"{rreplace(terminals,'|', '', 1)}\n")
--- interface.py
+++ interface.py
@@ -1,3 +1,5 @@
+import os, sys
+from kivy.resources import resource_add_path, resource_find
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
@@ -5,9 +7,9 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
-from kivy.properties import ObjectProperty
 from kivy.lang import Builder
 from kivy.core.window import Window
+from kivy.core.spelling import Spelling
 
 Builder.load_file("gui.kv")
 
@@ -19,51 +21,34 @@ class MyLayout(Widget):
 
     def __init__(self, **kwargs):
         super(MyLayout, self).__init__(**kwargs)
-        self.generated.bind(text = self.onTextChanged)
+        self.ids.original.bind(text=self.on_text)
 
-    def onAction(self):
-        if self.original.text.strip() != "":
-            self.generated.text = self.original.text * 3
+    def on_text(self, instance, value):
+        if self.ids.original.text.strip() != "":
+            self.ids.generated.text = self.ids.original.text
+        s = Spelling()
+        s.select_language("en_US")
+        if self.ids.original.text.strip() != "":
+            word = self.ids.original.text
+            options = s.suggest(word)
+            res = ""
+            for option in options:
+                res = f"{res} {option}"
+            self.ids.suggestions.text = res
     
     def onClear(self):
         self.generated.text = "Enter a sentence and click Generate Button to auto generate grammar"
         self.original.text = ""
- 
-    def onTextChanged(self, instance, text):
-        print("Something")
-        return self.ids.original.text
-'''        self.cols = 1
 
-        self.gl = GridLayout()
-        self.gl.cols = 2
-        self.add_widget(self.gl)
-
-        self.gl.add_widget(Label(text="Enter a sentence"))
-        self.original = TextInput(multiline=False)
-        self.gl.add_widget(self.original)
-
-        self.gl.add_widget(Label(text="Generated grammar"))
-        self.grammar = TextInput(multiline=False)
-        self.gl.add_widget(self.grammar)
-
-        self.submit = Button(
-            text="Generate", 
-            font_size=32,
-            size_hint_y = None,
-            height = 128
-            )
-        self.submit.bind(on_press = self.onAction)
-        self
-        self.add_widget(self.submit)
-
-    def onAction(self, intances):
-            self.grammar.text = self.original.text * 3'''
+    def selected(self, filename):
+        self.ids.icon.source = filename[0]
 
 
 class AutoGen(App):
 
     def build(self):
         Window.clearcolor = (224/255, 226/255, 219/255,1)
+        #Window.size = (800, 600)
         return MyLayout()
 
 if __name__ == "__main__":
